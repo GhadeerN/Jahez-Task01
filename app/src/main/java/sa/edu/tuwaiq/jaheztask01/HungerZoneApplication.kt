12d1d7101bd4f796d95ccbdf0394f723199ba679
Ghadeer <gadeer.a094@gmail.com>
2022-03-23 11:20:12 +0300

    Setup dependency injection with hilt

---
+++ app/src/main/java/sa/edu/tuwaiq/jaheztask01/HungerZoneApplication.kt
@@ -0,0 +1,9 @@
+package sa.edu.tuwaiq.jaheztask01
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HungerZoneApplication: Application() {
+
+}
--- app/src/main/java/sa/edu/tuwaiq/jaheztask01/presentation/MainActivity.kt
+++ app/src/main/java/sa/edu/tuwaiq/jaheztask01/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package sa.edu.tuwaiq.jaheztask01
+package sa.edu.tuwaiq.jaheztask01.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import sa.edu.tuwaiq.jaheztask01.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
