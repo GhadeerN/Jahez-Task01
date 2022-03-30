08e8115b1bbc598c361c554f87f51f206c2a35bf
Ghadeer <gadeer.a094@gmail.com>
2022-03-30 23:53:16 +0300

    Updated the UI, and deleted unnecessary files

--- app/src/main/java/sa/edu/tuwaiq/jaheztask01/domain/model/AuthState.kt
+++
@@ -1,7 +0,0 @@
-package sa.edu.tuwaiq.jaheztask01.domain.model
-
-data class AuthState(
-    val isLoading: Boolean = false,
-    val isSuccess: Boolean = false,
-    val error: String = ""
-)
--- app/src/main/java/sa/edu/tuwaiq/jaheztask01/domain/model/RestaurantState.kt
+++
@@ -1,9 +0,0 @@
-package sa.edu.tuwaiq.jaheztask01.domain.model
-
-import sa.edu.tuwaiq.jaheztask01.domain.model.RestaurantItem
-
-data class RestaurantState(
-    val isLoading: Boolean = false,
-    val restaurants: List<RestaurantItem> = emptyList(),
-    val error: String = ""
-)
